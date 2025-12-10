4305210fc222c07603763eb938d445bef4d9205d
Zhicheng <58725154+boss-yang702@users.noreply.github.com>
2025-12-10 17:18:21 +0800

    [fix] 位置参数未正确传递导致报错

--- training/dataset/albu.py
+++ training/dataset/albu.py
@@ -26,7 +26,7 @@ def isotropically_resize_image(img, size, interpolation_down=cv2.INTER_AREA, int
 class IsotropicResize(DualTransform):
     def __init__(self, max_side, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC,
                  always_apply=False, p=1):
-        super(IsotropicResize, self).__init__(always_apply, p)
+        super(IsotropicResize, self).__init__(p)
         self.max_side = max_side
         self.interpolation_down = interpolation_down
         self.interpolation_up = interpolation_up
